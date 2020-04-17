9ca9762a8716a2e6fae6402770e895f6110f7127
Marcos Mercado <marcoscoca0905@gmail.com>
2020-04-17 17:01:00 -0400

    Hace la secuencia

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 class Roulette{
-    constructor(green,red,blue,yellow){
-        this.red = red
-        this.blue = blue
-        this.green = green
-        this.yellow = yellow
+    constructor(colors,start){
+        this.start = start;
+        this.colors = colors;
+        this.level = 1;
+        this.colorSequence = [];
     }
 
-    start(){
-        console.log(this.red)
-        alert("INICIÓ");
+    sequence(){
+       // this.start.style.display = "none";
+        for(let i = 0; i < this.level; i++){
+           setTimeout(()=>{
+               this.turnOn(this.getRandomInt(0,this.colors.length),i)
+            },1000*i); 
+        }
+        this.check();
+    }
+    turnOn(color,i){
+        console.log(color)
+        this.colors[color].classList.remove("off");
+        setTimeout(()=>{this.colors[color].classList.add("off")},500);
+        this.colorSequence[i] = color;
+    }
+    getRandomInt(min, max){
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+    check(){
+        
     }
 }
 
 function startGame(){
     const container = document.querySelector(".container").querySelectorAll(".box");
-    console.log(container);
-    const myRoulette = new Roulette(...container);
-    myRoulette.start();
-
+    const start = document.querySelector(".start");
+    const myRoulette = new Roulette(container,start);
+    myRoulette.sequence();
 }
 
-const getRandomInt = (min, max)=> {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
 
 
