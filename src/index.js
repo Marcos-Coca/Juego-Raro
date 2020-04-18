6cf2f86cd65cfa64af920628ec1a5c25e49f1ef3
Marcos Mercado <marcoscoca0905@gmail.com>
2020-04-17 20:25:47 -0400

    Un poco de Avance

--- src/index.js
+++ src/index.js
@@ -2,39 +2,62 @@ class Roulette{
     constructor(colors,start){
         this.start = start;
         this.colors = colors;
-        this.level = 1;
+        this.level = 4;
+        
+    }
+    startGame(){
+        this.start.style.display = "none";
         this.colorSequence = [];
+        this.levelUp = 0;
+        this.container = document.querySelector(".container");
+        this.sequence();
+
     }
 
     sequence(){
-       // this.start.style.display = "none";
         for(let i = 0; i < this.level; i++){
            setTimeout(()=>{
                this.turnOn(this.getRandomInt(0,this.colors.length),i)
             },1000*i); 
         }
-        this.check();
+        this.listener();
     }
     turnOn(color,i){
         console.log(color)
         this.colors[color].classList.remove("off");
         setTimeout(()=>{this.colors[color].classList.add("off")},500);
-        this.colorSequence[i] = color;
+        this.colorSequence[i] = this.colors[color];
     }
     getRandomInt(min, max){
         return Math.floor(Math.random() * (max - min)) + min;
       }
 
-    check(){
-        
-    }
+    listener(){
+        this.container.addEventListener('click',e =>{
+            if(e.target != this.start){
+           if(this.colorSequence[this.levelUp] === e.target){
+                this.levelUp++;
+
+             }else{
+                 alert("FAIL")   
+             }
+        }
+        });
+    
 }
+
+    fail(){
+        alert("FAIL");
+    }
+
+}
+
 
 function startGame(){
     const container = document.querySelector(".container").querySelectorAll(".box");
     const start = document.querySelector(".start");
     const myRoulette = new Roulette(container,start);
-    myRoulette.sequence();
+    myRoulette.startGame();
 }
 
 
